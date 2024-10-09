9fb0bc5909fdd4e682ba11322e7e341f2a0db7a9
Matthew Zipkin <pinheadmz@gmail.com>
2024-10-09 18:58:14 -0400

    fix fleet

--- scripts/miner_wallet.sh
+++ scripts/miner_wallet.sh
@@ -1,2 +1,2 @@
 warnet bitcoin rpc miner createwallet miner
-warnet bitcoin rpc miner importdescriptors '[{\"desc\":\"combo\(cVSseQu6vAKax62CuYrMgbCtQRyFsjuhjQRFyYyUavKRdjQ1A1dV\)#4yxeyu6n\"\,\"timestamp\":0}]'
+warnet bitcoin rpc miner importdescriptors '[{\"desc\":\"combo\(cSS95s8qakXK2M58zu9PpqP4npSZfcUgf3YzshKi6BixceHTXP3G\)#09dmlg6k\"\,\"timestamp\":0}]'
